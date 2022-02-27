98e688e66001f0169807dc271c70dd4b301868ba
boussi ahmed <boussi.ahmed1920@gmail.com>
2022-02-27 16:18:59 +0100

    first commit

---
+++ src/main/java/com/example/demo/DevopsTestApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsTestApplication {
+	@RequestMapping(value="/")
+	public String index() {
+		return "<h1> hello word !<h1>";
+	}
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsTestApplication.class, args);
+		
+	}
+
+}
